0413f501dcb5fcde3c4c695d3dadcad2ffcc58c3
Christian Douglas <christian.douglas.alcantara@gmail.com>
2021-02-19 18:29:54 -0300

    📘 Book models and admin

--- apps/book/admin.py
+++ apps/book/admin.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "created", "modified")
+    list_filter = ("created", "modified")
+    search_fields = ("name",)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "created", "modified")
+    list_filter = ("created", "modified")
+    filter_horizontal = ["authors"]
--- apps/book/models.py
+++ apps/book/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Author(models.Model):
+    """
+    Author object
+    """
+
+    name = models.CharField(verbose_name=_("Name"), max_length=50)
+    created = models.DateTimeField(
+        verbose_name=_("Created"), editable=False, blank=True, auto_now_add=True
+    )
+    modified = models.DateTimeField(
+        verbose_name=_("Modified"), editable=False, blank=True, auto_now=True
+    )
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = _("Author")
+        verbose_name_plural = _("Authors")
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Book object
+    """
+
+    title = models.CharField(verbose_name=_("Title"), max_length=150, db_index=True)
+    description = models.TextField(verbose_name=_("description"))
+    authors = models.ManyToManyField(verbose_name=_("Authors"), to=Author)
+    created = models.DateTimeField(
+        verbose_name=_("Created"), editable=False, blank=True, auto_now_add=True
+    )
+    modified = models.DateTimeField(
+        verbose_name=_("Modified"), editable=False, blank=True, auto_now=True
+    )
+
+    class Meta:
+        ordering = ["-created", "title"]
+        verbose_name = _("Book")
+        verbose_name_plural = _("Books")
+
+    def __str__(self):
+        return self.title
