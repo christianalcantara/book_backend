5244663c7a7b3b58e9e796033b5a30995526c216
Christian Douglas <christian.douglas.alcantara@gmail.com>
2021-02-22 03:32:20 -0300

    add static/media url

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import logout
 from django.urls import path
@@ -13,3 +15,6 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api-token-auth/", views.obtain_auth_token),
 ]
+
+urlpatterns.append(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)[0])
+urlpatterns.append(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)[0])
